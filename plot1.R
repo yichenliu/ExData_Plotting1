ca17d514ef4b7ecc067b3030e6955cb4b7ed44f2
yichenliu <yichenliu@outlook.com>
2014-05-07 22:06:44 +0800

    Script Update

--- plot1.R
+++ plot1.R
@@ -1,11 +1,26 @@
+# Set working directory to the folder where data is located.
 setwd("C:/Project 1")
+
+# Read the data and the class of date as date.
 dt<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 dt[,1]<-as.Date(dt[,1],"%d/%m/%Y")
+
+# Create a new  data table where date are only 2007-02-01 and 2007-02-02,
+# and combine date and time together.
 dt<-rbind(dt[as.Date(dt[,1])=="2007-02-01",],dt[as.Date(dt[,1])=="2007-02-02",])
 Date_Time<-strptime(paste(dt[,1],dt[,2]), "%Y-%m-%d %H:%M:%S")
+
+# Create a new data table for plotting.
 dt<-cbind(Date_Time,dt[,c(3:9)]) 
 
+# Call "png" graphic device and save the plot as "plot1.png".
 png(file="plot1.png")
+
+# Set plot global background colour as "transparent".
 par(bg="transparent")
+
+# Plot the histogram.
 hist(dt$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+
+# Turn off the graphic device.
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,12 +1,27 @@
+# Set working directory to the folder where data is located.
 setwd("C:/Project 1")
+
+# Read the data and the class of date as date.
 dt<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 dt[,1]<-as.Date(dt[,1],"%d/%m/%Y")
+
+# Create a new  data table where date are only 2007-02-01 and 2007-02-02,
+# and combine date and time together.
 dt<-rbind(dt[as.Date(dt[,1])=="2007-02-01",],dt[as.Date(dt[,1])=="2007-02-02",])
 Date_Time<-strptime(paste(dt[,1],dt[,2]), "%Y-%m-%d %H:%M:%S")
+
+# Create a new data table for plotting.
 dt<-cbind(Date_Time,dt[,c(3:9)]) 
 
+# Call "png" graphic device and save the plot as "plot2.png".
 png(file="plot2.png")
+
+# Set plot global background colour as "transparent".
 par(bg="transparent")
+
+# Set up the chart and plot the line.
 plot(dt$Date_Time,dt$Global_active_power,xlab="",ylab="Global Active Power (kilowatts)",type="n")
 lines(dt$Date_Time,dt$Global_active_power)
+
+# Turn off the graphic device.
 dev.off()
