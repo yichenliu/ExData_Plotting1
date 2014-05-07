66cbe6170c929cfd82470ef8b18b8fbb38bef88e
yichenliu <yichenliu@outlook.com>
2014-05-07 22:16:38 +0800

    Script Update

--- plot3.R
+++ plot3.R
@@ -1,11 +1,11 @@
 # Set working directory to the folder where data is located.
 setwd("C:/Project 1")
 
-# Read the data and the class of date as date.
+# Read the data and set the class of date as date.
 dt<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 dt[,1]<-as.Date(dt[,1],"%d/%m/%Y")
 
-# Create a new  data table where date are only 2007-02-01 and 2007-02-02,
+# Create a new data table where dates are only 2007-02-01 and 2007-02-02,
 # and combine date and time together.
 dt<-rbind(dt[as.Date(dt[,1])=="2007-02-01",],dt[as.Date(dt[,1])=="2007-02-02",])
 Date_Time<-strptime(paste(dt[,1],dt[,2]), "%Y-%m-%d %H:%M:%S")
